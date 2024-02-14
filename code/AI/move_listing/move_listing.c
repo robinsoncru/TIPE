8fd41d0e9da6c7b07c3050d4d7c751b37375405e
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2024-02-14 20:28:29 +0100

    lister les coups de move back

--- code/AI/move_listing/move_listing.c
+++ code/AI/move_listing/move_listing.c
@@ -1,2 +1,49 @@
 #include "move_listing.h"
+#include "move_max.h"
+#include <stdlib.h>
 
+Move* listMovesMoveBack(Game* g, int* resSize){
+    pawn pawnToMove = g->allPawns[g->is_white][g->ind_move_back];
+    int i = pawnToMove.lig, j = pawnToMove.col;
+    int perspective = (g->is_white) ? 1 : -1;
+
+    Move* temporaryResult = malloc(2 * sizeof(Move));
+    Move currentMove;
+    int nbMoves = 0;
+
+    currentMove.type = pawnMoveBackType;
+    currentMove.manipulatedPawn = g->ind_move_back;
+
+    int colShift[2] = {-1, 1};
+    int newLig = i - perspective;
+    for (int k = 0; k < 2; k++) {
+        if (canMoveBack(g, g->is_white, g->ind_move_back, k != 1)) {
+            currentMove.left = k != 1;
+            temporaryResult[nbMoves] = currentMove;
+            nbMoves++;
+        }
+    }
+
+    *resSize = nbMoves;
+    return temporaryResult;
+}
+
+MoveTab* listMoves(Game* g){
+    Move* temporaryResult;
+    int nbMoves;
+    if (g->ind_move_back == VOID_INDEX) {
+        temporaryResult = listMovesMoveBack(g, &nbMoves);
+    }
+    else {
+        temporaryResult = malloc(maxMoves(g) * sizeof(Move));
+        nbMoves = 0;
+    }
+
+    MoveTab* actualResult = malloc(sizeof(MoveTab));
+    actualResult -> size = nbMoves;
+    for (int k = 0; k < nbMoves; k++) {
+        actualResult->tab[k] = temporaryResult[k];
+    }
+    free(temporaryResult);
+    return actualResult;
+}
--- code/AI/move_listing/move_listing.h
+++ code/AI/move_listing/move_listing.h
@@ -1,16 +1,24 @@
 #ifndef MOVE_LISTING
 #define MOVE_LISTING
 
+#include <stdlib.h>
 #include "../../fundamental_functions/game_functions/Source/source_header.h"
+#include "../../fundamental_functions/game_functions/Logic_and_calculus/logic_functions.h"
+#include "move_max.h"
 
 enum moveType{pawnMoveType, promotionType, pawnMoveBackType, biDeplType, queenDeplType, rafleType, lienAmitieType, lienEnnemitieType};
 
 typedef struct Move{
     enum moveType type;
-
     int manipulatedPawn;
+
+    //wether or not the movement of the pawn
+    //is made to the left, used in moving and
+    //moving back
     bool left;
+
     queen_move_t coords;
+
     //selected pawn on the other side for
     //friendship/hate creation
     int lig, col;
