92bfd0621581188679d1ce2346ecfeee91bf64dc
Temboya NDougala <t3mb0ya.mlg@gmail.com>
2024-05-25 01:44:07 +0200

    optimisier dag de dependances

--- code/AI/move_listing/move_listing_debug/debug_autoplay.h
+++ code/AI/move_listing/move_listing_debug/debug_autoplay.h
@@ -1,7 +1,9 @@
 #ifndef DEBUG_AUTOPLAY
 #define DEBUG_AUTOPLAY
 
-#include "../actions/input_command.h"
+#include "../move_struct/move_struct.h"
+#include "../../../fundamental_functions/debug/debug.h"
+#include <stdlib.h>
 
 void print_move(Move m);
 void print_moves(MoveTab *t);
